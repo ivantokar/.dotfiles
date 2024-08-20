e696638c3e5a234e273180db042e84a888316fb3
Ivan Tokar <i.j.tokar@gmail.com>
2024-08-20 22:33:25 +0300

    added arrow plugin

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -113,4 +113,12 @@ return {
             },
         },
     },
+    {
+        "otavioschwanck/arrow.nvim",
+        opts = {
+            show_icons = true,
+            leader_key = ";", -- Recommended to be a single key
+            buffer_leader_key = "m", -- Per Buffer Mappings
+        },
+    },
 }
