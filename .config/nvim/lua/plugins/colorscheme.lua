dd8cce83e94a31d761cda121c6dbc52d87160f97
Ivan Tokar <i.j.tokar@gmail.com>
2025-01-02 15:42:55 +0200

    added config for ghostty

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -9,8 +9,7 @@ return {
         require("rose-pine").setup({
             variant = "auto", -- auto, main, moon, or dawn
             dark_variant = "main", -- main, moon, or dawn
-            light_variant = "dawn", -- main, moon, or dawn
-            -- dim_inactive_windows = true,
+            dim_inactive_windows = true,
             extend_background_behind_borders = true,
 
             styles = {
