ca2cdcdf7e6162c5f331f2dfa1f95ccc495aa7d4
Ivan Tokar <i.j.tokar@gmail.com>
2025-02-14 18:03:44 +0200

    migration to new lapton

---
+++ .config/nvim/lua/plugins/ui/tabs.lua
@@ -0,0 +1,13 @@
+return {
+	-- {
+	-- 	"akinsho/bufferline.nvim",
+	-- 	version = "*",
+	-- 	dependencies = "nvim-tree/nvim-web-devicons",
+	--
+	-- 	config = function()
+	-- 		require("bufferline").setup({})
+	--
+	-- 		vim.opt.termguicolors = true
+	-- 	end,
+	-- },
+}
