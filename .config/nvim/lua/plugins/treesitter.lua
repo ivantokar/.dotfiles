7e1def8ad0d518e7428bbb0c90a30245bb240fc2
Ivan Tokar <ivantokar@Ivans-MacBook-Pro.local>
2025-04-17 11:08:28 +0300

    fix

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,43 +7,13 @@ return {
 				-- A list of parser names, or "all"
 				ensure_installed = {
 					"vimdoc",
-<<<<<<< HEAD
 					"javascript",
 					"typescript",
 					"c",
-=======
-                    'regex',
-					"c",
-					"swift",
->>>>>>> d26398d2152baf0f825be4b2dfc9f2ddfbe01fab
 					"lua",
 					"jsdoc",
 					"bash",
 					"graphql",
-<<<<<<< HEAD
-					"markdown",
-					"markdown_inline",
-					"html",
-					"twig",
-					"css",
-					"scss",
-					"json",
-					"yaml",
-					"toml",
-					"python",
-					"java",
-					"rust",
-					"php",
-					"cpp",
-					"query",
-					"graphql",
-=======
-					"javascript",
-					"typescript",
-					"markdown",
-					"markdown_inline",
-					"html",
->>>>>>> d26398d2152baf0f825be4b2dfc9f2ddfbe01fab
 				},
 
 				-- Install parsers synchronously (only applied to `ensure_installed`)
@@ -65,7 +35,6 @@ return {
 					-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
 					-- Using this option may slow down your editor, and you may see some duplicate highlights.
 					-- Instead of true it can also be a list of languages
-<<<<<<< HEAD
 					additional_vim_regex_highlighting = false,
 				},
 			})
@@ -81,22 +50,6 @@ return {
 					stencil = "twig",
 				},
 			})
-=======
-					additional_vim_regex_highlighting = { "markdown" },
-				},
-			})
-
-			local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-			treesitter_parser_config.templ = {
-				install_info = {
-					url = "https://github.com/vrischmann/tree-sitter-templ.git",
-					files = { "src/parser.c", "src/scanner.c" },
-					branch = "master",
-				},
-			}
-
-			vim.treesitter.language.register("templ", "templ")
->>>>>>> d26398d2152baf0f825be4b2dfc9f2ddfbe01fab
 		end,
 	},
 }
