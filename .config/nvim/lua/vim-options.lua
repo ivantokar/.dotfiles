1b74e0f482cd36aadd55bce3efb33aa4b339a975
Ivan Tokar <i.j.tokar@gmail.com>
2025-09-16 13:46:48 +0300

    updated ghostty

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,28 +1,28 @@
 -- Basic
-vim.opt.nu = true                                      -- Line numbers
-vim.opt.relativenumber = true                          -- Relative line numbers
-vim.opt.tabstop = 4                                    -- Number of spaces tabs count for
-vim.opt.softtabstop = 4                                -- Number of spaces tabs count for
-vim.opt.shiftwidth = 4                                 -- Size of an indent
-vim.opt.expandtab = true                               -- Use spaces instead of tabs
-vim.opt.smartindent = true                             -- Smart indentation
-vim.opt.wrap = false                                   -- No wrap
-vim.opt.swapfile = false                               -- No swap files
-vim.opt.backup = false                                 -- No backup files
+vim.opt.nu = true -- Line numbers
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.tabstop = 4 -- Number of spaces tabs count for
+vim.opt.softtabstop = 4 -- Number of spaces tabs count for
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Smart indentation
+vim.opt.wrap = false -- No wrap
+vim.opt.swapfile = false -- No swap files
+vim.opt.backup = false -- No backup files
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- Save undo history
-vim.opt.undofile = true                                -- Save undo history
-vim.opt.hlsearch = false                               -- Highlight search results
-vim.opt.incsearch = true                               -- Incremental search
-vim.opt.termguicolors = true                           -- True color support
-vim.opt.scrolloff = 8                                  -- Lines of context
-vim.opt.signcolumn = "yes"                             -- Always show sign column
-vim.opt.isfname:append("@-@")                          -- Allow @ in file names
-vim.opt.updatetime = 50                                -- Faster completion
-vim.opt.colorcolumn = "80"                             -- Show a column at 80 characters
-vim.opt.cursorline = true                              -- Highlight the current line
-vim.opt.showmode = true                                -- Show the current mode
-vim.opt.spell = true                                   -- Enable spell check
-vim.opt.spelllang = "en_us"                            -- Set spell check language
+vim.opt.undofile = true -- Save undo history
+vim.opt.hlsearch = false -- Highlight search results
+vim.opt.incsearch = true -- Incremental search
+vim.opt.termguicolors = true -- True color support
+vim.opt.scrolloff = 8 -- Lines of context
+vim.opt.signcolumn = "yes" -- Always show sign column
+vim.opt.isfname:append("@-@") -- Allow @ in file names
+vim.opt.updatetime = 50 -- Faster completion
+vim.opt.colorcolumn = "80" -- Show a column at 80 characters
+vim.opt.cursorline = true -- Highlight the current line
+vim.opt.showmode = true -- Show the current mode
+vim.opt.spell = true -- Enable spell check
+vim.opt.spelllang = { "en_us" } -- Set spell check language
 
 -- Set leader key
 vim.g.mapleader = " "
@@ -33,5 +33,5 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
 -- Paste from system clipboard multiple times
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
